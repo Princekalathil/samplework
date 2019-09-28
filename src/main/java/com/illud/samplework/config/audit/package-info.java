9c53a8b96f38c12107d815469fc67fb559be6625
Princekalathil <prince.k.c@lxisoft.com>
2019-09-28 11:07:06 +0530

    Initial application generated by JHipster-5.8.2

---
+++ src/main/java/com/illud/samplework/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.illud.samplework.config.audit;
---
+++ src/main/java/com/illud/samplework/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.illud.samplework.repository.search;
+
+import com.illud.samplework.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, String> {
+}
